9224785ce7a9b4a37af7d2cb750b18eef9f01c78
hossensumon071 <hossensumon071@gmail.com>
2021-03-25 11:20:40 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html webpage in php</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <style>
+        body{
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h1>welcome</h1>
+                <h2>
+                  <?php echo "hello world"; ?>
+                </h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum unde accusantium odio, impedit doloremque deserunt rerum eaque, excepturi repellat perferendis, quisquam fugiat voluptas commodi. Inventore rerum, quibusdam porro quo voluptas optio tempore nisi praesentium maiores officia odio labore! Ex rerum ratione quae autem? Modi exercitationem similique dolore, adipisci fuga tempora.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
